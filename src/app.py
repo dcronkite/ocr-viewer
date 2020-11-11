d5c175545ba40415d8aeb28932f56751e590c08d
dcronkite <dcronkite@gmail.com>
2020-11-10 16:25:02 -0800

    Added save option

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
+import datetime
 import pathlib
+import pickle
+import sqlite3
 import tkinter as tk
+import random
+import uuid
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
@@ -11,6 +16,10 @@ class Application(tk.Frame):
         self.master = master
         self.pack(fill=tk.BOTH, expand=1)
 
+        #
+        self.conn = sqlite3.connect('ocr_viewer.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        self.init_sqlite()
+
         # variables
         self.records = []
         self.records_index = 0
@@ -20,6 +29,18 @@ class Application(tk.Frame):
 
         # create widgets
         self.create_widgets()
+
+    def init_sqlite(self):
+        cur = self.conn.cursor()
+        cur.execute('''
+        create table if not exists project 
+        (id integer primary key,
+         name text,
+         uuid text,
+         date timestamp
+        )
+        ''')
+        self.conn.commit()
 
     def create_widgets(self):
 
@@ -31,14 +52,24 @@ class Application(tk.Frame):
         self.btn_browse_folder = tk.Button(self, text="Browse Folder", command=self.set_folder_path)
         self.btn_browse_folder.grid(row=1, column=11)
 
-        self.btn_retrieve = tk.Button(self, text='Retrieve Records', command=self.get_records)
+        self.btn_retrieve = tk.Button(self, text='(Re-)Load Records', command=self.get_records)
         self.btn_retrieve.grid(row=2, column=0, columnspan=4, rowspan=1)
         self.record_cnt = tk.IntVar()
         self.cnt_entry = tk.Entry(self, textvariable=self.record_cnt, state=tk.DISABLED)
         self.cnt_entry.grid(row=2, column=4)
 
+        self.save_label = tk.Label(self, text='Project Name')
+        self.save_label.grid(row=2, column=5, columnspan=2)
+        self.save_project_name = tk.StringVar()
+        self.save_entry = tk.Entry(self, textvariable=self.save_project_name, width=35)
+        self.save_entry.grid(row=2, column=7, columnspan=2)
+        self.save_btn = tk.Button(self, text='Save', command=self.save)
+        self.save_btn.grid(row=2, column=9, columnspan=2, rowspan=1)
+
         self.btn_prev = tk.Button(self, text='Previous Record', command=self.get_previous_record)
         self.btn_prev.grid(row=3, column=0, columnspan=3)
+        self.btn_next = tk.Button(self, text='Shuffle', command=self.shuffle_records)
+        self.btn_next.grid(row=3, column=4, columnspan=1)
         self.btn_next = tk.Button(self, text='Next Record', command=self.get_next_record)
         self.btn_next.grid(row=3, column=5, columnspan=3)
 
@@ -55,7 +86,16 @@ class Application(tk.Frame):
     def set_folder_path(self):
         path = filedialog.askdirectory()
         self.ocr_doc_path = pathlib.Path(path)
-        self.ocr_doc_path_var.set(filedialog.askdirectory())
+        self.ocr_doc_path_var.set(path)
+
+    def save(self):
+        cur = self.conn.cursor()
+        uid = uuid.uuid4()
+        cur.execute('insert into project (name, uuid, date) values (?, ?, ?)',
+                    (self.save_project_name.get() or 'UNKNOWN', str(uid), datetime.datetime.now()))
+        self.conn.commit()
+        with open(f'{uid}', 'wb') as fh:
+            pickle.dump((self.records, self.record_to_text, self.record_to_image), fh, protocol=4)  # hard-code version
 
     def get_records(self):
         if not self.ocr_doc_path:
@@ -72,6 +112,9 @@ class Application(tk.Frame):
         self.records = list(recs)
         self.record_cnt.set(len(self.records))
         self.records_index = -1
+
+    def shuffle_records(self):
+        random.shuffle(self.records)
 
     def _get_image(self, rec):
         img_path = self.record_to_image.get(rec, None)
